ccfd954c51cfaab0d7b91c60babb5b600495c638
merinovvvv <87500057+merinovvvv@users.noreply.github.com>
2026-07-07 17:00:01 +0300

    clear project is added

---
+++ CodeBreaker/CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Yaraslau Merynau on 07.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
